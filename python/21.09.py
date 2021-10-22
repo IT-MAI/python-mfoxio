e2cf96a984eedda8f344d8cf079a46514b2d037f
Anton <91245910+mfoxio@users.noreply.github.com>
2021-10-22 20:23:21 +0300

    Add files via upload

---
+++ python/21.09.py
@@ -0,0 +1,34 @@
+#1)
+a = int(input())
+b = int(input())
+print(a,b)
+a,b = b,a
+print(a,b)
+
+#2)
+a=int(input())
+b=a//100
+c=a%10
+d=(a-100*b-c)/10
+x=b*c*d
+print (x)
+
+#3)
+a=int(input())
+b=int(input())
+c=1000*a+b
+print (c)
+
+#4)
+a=int(input())
+b=a//100
+c=a%10
+x=100*b+c
+print (x)
+
+#5)
+a=int(input())
+b=a//100
+c=a%10
+x=100*b+c
+print (x)
